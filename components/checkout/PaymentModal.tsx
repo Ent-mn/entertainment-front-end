c9c2481b3197a75be0c3f25d7971337ae66e5837
Bilguun <Bilguun040728@gmail.com>
2025-03-27 18:46:17 +0800

    gg

--- components/checkout/PaymentModal.tsx
+++ components/checkout/PaymentModal.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import BookingPage from "./Test";
-import FeaturesSection from "./CheckoutMain";
 
 const PaymentModal = () => {
   return (
     <div className="">
-      {/* <FeaturesSection /> */}
       <BookingPage />
     </div>
   );
