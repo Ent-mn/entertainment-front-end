6ba2347d4cd7ead5298c2bffbf9c19fff38ba831
Sukhtumur <it@apex.mn>
2025-05-12 10:56:20 +0800

    asd

--- components/RestRegister.tsx
+++ components/RestRegister.tsx
@@ -8,6 +8,7 @@ import { Button } from "./ui/button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import RestLogin from "./RestLogin";
+import { signIn } from "next-auth/react";
 
 import {
   Dialog,
@@ -322,6 +323,9 @@ const RestRegister = () => {
                     <div className="grid grid-cols-2 gap-3 mt-4 h-9 w-72">
                       <button
                         type="button"
+                        onClick={() =>
+                          signIn("facebook", { callbackUrl: "/restaurant" })
+                        }
                         className="flex items-center justify-center h-9 px-1 border border-[#e0e0e0] rounded-xl bg-white"
                       >
                         <svg
@@ -333,41 +337,43 @@ const RestRegister = () => {
                         >
                           <path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C18.34 21.21 22 17.06 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
                         </svg>
-                        <span className="text-[#8c8c8c] text-[10px]">
-                          Sign in with Facebook
+                        <span className="text-[#8c8c8c] text-[10px] ml-1">
+                          Sign up with Facebook
                         </span>
                       </button>
+
                       <button
                         type="button"
+                        onClick={() =>
+                          signIn("google", { callbackUrl: "/restaurant" })
+                        }
                         className="flex items-center justify-center h-9 gap-1 px-1 border border-[#e0e0e0] rounded-xl bg-white"
                       >
-                        <div className="mr-2 flex h-5 w-5 items-center justify-center">
-                          <svg
-                            width="18"
-                            height="18"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                              fill="#4285F4"
-                            />
-                            <path
-                              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                              fill="#34A853"
-                            />
-                            <path
-                              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                              fill="#FBBC05"
-                            />
-                            <path
-                              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                              fill="#EA4335"
-                            />
-                          </svg>
-                        </div>
+                        <svg
+                          width="18"
+                          height="18"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                            fill="#4285F4"
+                          />
+                          <path
+                            d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                            fill="#34A853"
+                          />
+                          <path
+                            d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                            fill="#FBBC05"
+                          />
+                          <path
+                            d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                            fill="#EA4335"
+                          />
+                        </svg>
                         <span className="text-[#8c8c8c] text-[10px]">
-                          Sign in with Google
+                          Sign up with Google
                         </span>
                       </button>
                     </div>
